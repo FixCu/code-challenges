d70fafedc806e62ada7c1168053833617d1dc0b1
carterror <carlos.bramila98@gmail.com>
2024-03-07 22:25:44 -0500

    test p1-s1

--- p1/s1.py
+++ p1/s1.py
@@ -2,7 +2,11 @@
 import math 
  
 
-def msuma(lista): 
+def msuma(lista):
+    if len(lista) == 0:
+        return 0
+    if len(lista) == 1:
+        return lista[0]
     mayor = -math.inf 
     for i in range(len(lista)-1): 
         res = lista[i] + lista[i+1] 
@@ -10,6 +14,11 @@ def msuma(lista):
             mayor = res  
     return mayor 
 
-assert msuma([6, 3 , 7, 2, 7, 3, 9, 10, 1, 2, 4]) == 193
+assert msuma([6, 3 , 7, 2, 7, 3, 9, 10, 1, 2, 4]) == 19
+assert msuma([]) == 0
+assert msuma([6, 3 , 7, 2, 7, 3, 9, 10, 1, 2, 4, 10, 10]) == 20
+assert msuma([10, 10 , 7, 2, 7, 3, 9, 10, 1, 2, 4]) == 20
+assert msuma([10]) == 10
+assert msuma([10, 10]) == 20
 
 print('great!!!')
