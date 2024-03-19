7c3ef4ed4bee65919d2e7d48ec65fa3c0c0b6288
Jjnez00 <jgjnez@gmail.com>
2024-03-19 16:13:42 -0400

    Modified JjnezTest

--- problem_0004/Jjnez/JjnezTesr.java
+++ problem_0004/Jjnez/JjnezTesr.java
@@ -13,12 +13,12 @@ public class JjnezTest {
     @Test
     public void testTest1() {
         Integer result = Jjnez.duplicate([1, 2, 3, 4]);
-        assertInteger(1)
+        assertInteger(1);
     }
     
     @Test
     public void testTest2() {
         Integer result = Jjnez.duplicate([6, 3, 7, 2, 7, 3, 9, 10, 1, 2, 4]);
-        assertInteger(7)
+        assertInteger(7);
     }
 } 
