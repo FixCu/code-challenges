38149cc76826f859111f9f7ef76f1c0f1954037e
Jjnez00 <jgjnez@gmail.com>
2024-03-19 16:17:58 -0400

    Update solution

--- problem_0004/Jjnez/Jjnez.java
+++ problem_0004/Jjnez/Jjnez.java
@@ -7,13 +7,13 @@ public class Main{
     
 	public static void main(String[] args) {
 		
-		List<Integer> numbers1 = List.of(1, 2, 3, 4, 3, 1, 2);
+		List<Integer> numbers = List.of(1, 2, 3, 4, 3, 1, 2);
+        System.out.println(duplicate(numbers));
 		       
 	}
 	
 	public static Integer duplicate( List<Integer> numbers ) {
 	    Set<Integer> set = new HashSet<>();
-        List<Object> duplicates = new ArrayList<>();
         
         for (int i=0; i<numbers.size(); i++) {
             if(!set.add(numbers.get(i))) {
